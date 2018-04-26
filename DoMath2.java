4e4530f04a214ef8ab25e03d3fff94a4256514e9
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2018-04-26 17:38:18 +0200

    Add files via upload

---
+++ DoMath2.java
@@ -0,0 +1,12 @@
+class DoMath2{//komentarze jak w c
+	public static void main(String[] arguments){
+	//Wykonanie programu zaczyna siê tutaj
+	double score=1.0+2.0*3.0;
+	System.out.println(score);
+	double copy=score;
+	copy=copy/2.0;
+	System.out.println(copy);
+	System.out.println(score);
+	}
+
+}
