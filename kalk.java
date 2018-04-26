385a6b4d173fa34046a437c9c0fef171369f3cc4
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2018-04-26 17:51:56 +0200

    Add files via upload

---
+++ kalk.java
@@ -0,0 +1,13 @@
+class kalk{//komentarze jak w c 
+	//IntelliJDEA zainstalowaæ
+	public static void main(String[] arguments){
+	//Wykonanie programu zaczyna siê tutaj
+	double score=0.5*9.81*5*5+10;
+	System.out.println(score);
+	//double copy=score;
+	//copy=copy/2.0;
+	//System.out.println(copy);
+	//System.out.println(score);
+	}
+
+}
