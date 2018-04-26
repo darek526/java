67e38ec1091f56eda39c70a035ca57d785a4e1e3
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2018-04-26 17:35:20 +0200

    Add files via upload

---
+++ DoMath.java
@@ -0,0 +1,10 @@
+class DoMath{//komentarze jak w c
+	public static void main(String[] arguments){
+	//Wykonanie programu zaczyna siê tutaj
+	double score=1.0+2.0*3.0;
+	System.out.println(score);
+	score=score/2.0;
+	System.out.println(score);
+	}
+
+}
