50e4b6ad2aa6c12fabc623efde25a735bec426b1
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2018-04-26 17:24:26 +0200

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,8 @@
+class Hello2{//ghjkg
+	public static void main(String[] arguments){
+	//Wykonanie programu zaczyna siê tutaj
+	System.out.println("Hello world.");
+	System.out.println("Line number 2");
+	}
+
+}
