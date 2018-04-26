52d0cfbb14e0975df7dd19d179c0873fa864c331
Dariusz Soborski <32923506+darek526@users.noreply.github.com>
2018-04-26 17:30:00 +0200

    Add files via upload

---
+++ Hello3.java
@@ -0,0 +1,10 @@
+class Hello3{//ghjkg
+	public static void main(String[] arguments){
+	//Wykonanie programu zaczyna siê tutaj
+	String foo="IAP 6.092";
+	System.out.println(foo);
+	foo="Cos tam";
+	System.out.println(foo);
+	}
+
+}
